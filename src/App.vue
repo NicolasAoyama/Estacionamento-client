390c732c13a0407a38e9662d04f048b1ad2d9543
Nicolas <nicolasyuj@hotmail.com>
2023-06-20 17:38:28 -0300

    Fazendo as vues

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Estacionamento Mercosul</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/"><a class="nav-link" href="#">Home</a></router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about"><a class="nav-link" href="#">Sobre</a></router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/condutor"><a class="nav-link" href="#">Condutor</a></router-link>
+            </li>
+            
+            <li class="nav-item">
+              <router-link to="/configuracao"><a class="nav-link" href="#">Configuracao</a></router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
+// Variable overrides first
+$primary: #900;
+$enable-shadows: true;
+$prefix: "mo-";
+
+// Then import Bootstrap
+@import "../node_modules/bootstrap/scss/bootstrap";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/Configuracao.vue
@@ -0,0 +1,80 @@
+<template>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Estacionamento Mercosul</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
+    </head>
+    <body>
+      <div class="container">
+    
+    
+        <div class="row">
+          <div class="col-lg-4"></div>
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Configuracao</h5>
+                <form>
+                  <div class="form-group">
+                    <label for="nome">Valor Hora</label>
+                    <input type="text" class="form-control" id="nome">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Horadio de inicio do expediente</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Horadio de fim do expediente</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Tempo Para o Desconto</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Tempo De Desconto</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Gerar Desconto</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Vagas Carro</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Vagas Moto</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <div class="form-group">
+                    <label for="placa">Vagas Van</label>
+                    <input type="text" class="form-control" id="placa">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Cadastrar</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <footer class="mt-5">
+          <p>&copy; 2023 Estacionamento Mercosul. Todos os direitos reservados.</p>
+        </footer>
+      </div>
+    </body>
+    </template>
+    
+    <script lang="ts">
+    import { defineComponent } from 'vue';
+    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    
+    export default defineComponent({
+      name: 'HomeView',
+      components: {
+        HelloWorld,
+      },
+    });
+    </script>
+    
